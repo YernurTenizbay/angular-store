0a66d0ee12b9156f0f9e87834cc3995144a7d08b
YernurTenizbay <ernur.tenizbay@mail.ru>
2021-03-02 11:07:37 +0600

    1 file modified

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -29,7 +29,10 @@ export class ProductListComponent implements OnInit{
   ngOnInit() {
    const routeParams = this.route.snapshot.paramMap;
    const categorysIdFromRoute = Number(routeParams.get('categorysId'));
+   const productIdFromRoute = Number(routeParams.get('productId'));
 
+  // Find the product that correspond with the id provided in route.
+   this.product = products.find(product => product.id === productIdFromRoute);
     // Find the product that correspond with the id provided in route.
    this.categorys = category.find(categorys => categorys.id === categorysIdFromRoute);
   }
